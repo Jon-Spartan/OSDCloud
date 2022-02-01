d08ccc0b313c6a0376a8a9d17d343008d4e8e002
Jon-Spartan <98754338+Jon-Spartan@users.noreply.github.com>
2022-02-01 08:40:54 +0000

    Update OSDCloud-Config.ps1

--- OSDCloud-Config.ps1
+++ OSDCloud-Config.ps1
@@ -23,7 +23,25 @@ Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
 Start-OSDCloud -OSLanguage en-gb -OSBuild 21H1 -OSEdition Pro -ZTI
 
 #Install Windows updates - not working
+Set-ExecutionPolicy -bypass
+$UpdateWindows =$true
+if (!(Get-Module PSWindowsUpdate -ListAvailable)) {
+    try {
+        Install-Module PSWindowsUpdate -Force
+    }
+    catch {
+        Write-Warning 'Unable to install PSWindowsUpdate Powershell Module'
+        $UpdateWindows = $false
+    }
+}
 
+if ($UpdateWindows) {
+    Write-Host -Foregroundcolor DarkCyan 'Add-WUServiceManager -MicrosoftUpdate -Confirm:$false'
+    Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+    
+    Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Malicious'
+}
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
